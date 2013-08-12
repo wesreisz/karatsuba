4f447cd1eb098ded095b18aaa8952e7b6d5436ea
Wesley Reisz <wes@wesleyreisz.com>
2013-08-11 21:23:23 -0400

    updated using biginterger

--- Karatsuba/src/com/wesleyreisz/algorithms/karatsuba/simple/Karatsuba.java
+++ Karatsuba/src/com/wesleyreisz/algorithms/karatsuba/simple/Karatsuba.java
@@ -24,19 +24,19 @@ public class Karatsuba {
         long step4 = step3-step2-step1;
         
         //add the results
-        long result = 0;
-        result = padWithZeros(step1, String.valueOf(step3).length());
-        result = result + step2;
-        result = result + padWithZeros(step4,  String.valueOf(step3).length()/2 + (String.valueOf(step3).length() % 2));
+        BigInteger result = BigInteger.valueOf(0);
+        result = result.add(padWithZeros(step1, String.valueOf(step3).length()));
+        result = result.add(BigInteger.valueOf(step2));
+        result = result.add(padWithZeros(step4,  String.valueOf(step3).length()/2 + (String.valueOf(step3).length() % 2)));
         
-        return BigInteger.valueOf(result);
+        return result;
 	}
-	private static long padWithZeros(long value, long num2pad){
+	private static BigInteger padWithZeros(long value, long num2pad){
 		String result =String.valueOf(value);
 		for(int cnt=0;cnt<num2pad;cnt++){
 			result+="0";
 		}
-		return Integer.parseInt(result);
+		return BigInteger.valueOf(Long.parseLong(result));
 	}
 	private static char[] getSubString(char[] chars, int start, int length){
 		char[]result = new char[length-start];
--- Karatsuba/src/com/wesleyreisz/algorithms/karatsuba/simple/KaratsubaTest.java
+++ Karatsuba/src/com/wesleyreisz/algorithms/karatsuba/simple/KaratsubaTest.java
@@ -8,11 +8,12 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class KaratsubaTest {
+	private static int BIT_SIZE = 8;
 	@Test
 	public void test() {
 		Random rand = new Random();
-		BigInteger a = BigInteger.valueOf(1234);//new BigInteger(BIT_SIZE,rand);
-		BigInteger b = BigInteger.valueOf(5678);//new BigInteger(BIT_SIZE,rand);
+		BigInteger a = new BigInteger(BIT_SIZE,rand);
+		BigInteger b = new BigInteger(BIT_SIZE,rand);
 		
 		BigInteger testResult =  Karatsuba.multiply(a,b);
 		System.out.println(String.format("%s * %s = %s", a , b, testResult));
